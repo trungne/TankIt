30a6fb2debc7ed802ce473fba3ddf9d594b37878
Trung Nguyen <trungnguyen@Tans-MacBook-Pro.local>
2020-04-23 12:38:44 +0700

    hello

--- TankIt.py
+++ TankIt.py
@@ -2,19 +2,22 @@ import pygame
 
 pygame.init()
 # basic display
-window = (800, 800)
+window = (512, 512)
 win = pygame.display.set_mode(window)
 pygame.display.set_caption("TankIt")
 
-
 # images file import:
+bg = pygame.image.load('grass00.png')
+facesideway = pygame.image.load('tankBasesideway.png')
+faceupdown = pygame.image.load('tankBase.png')
 
 
 # character - tank
 class Tank:
-    def __init__(self, location, size, vel, left=False, right=False, up=False, down=False):
+    def __init__(self, location, width, height, vel, left=False, right=False, up=False, down=False):
         self.location = location
-        self.size = size  # default 0 = width, 1 = height
+        self.width = width
+        self.height = height
         self.vel = vel
         self.left = left
         self.right = right
@@ -31,31 +34,16 @@ class Tank:
         elif self.down:
             self.location[1] += self.vel
 
-    def turn(self):  # left + right
-        if not self.left:
-            self.location[0] = self.location[0] - abs(self.size[1] - self.size[0]) // 2  # x coordinate
-            self.location[1] = self.location[1] + abs(self.size[1] - self.size[0]) // 2  # y coordinate
-            if self.size[0] < self.size[1]:
-                self.size[0], self.size[1] = self.size[1], self.size[0]
-        if not self.right:
-            self.location[0] = self.location[0] - abs(self.size[1] - self.size[0]) // 2  # x coordinate
-            self.location[1] = self.location[1] + abs(self.size[1] - self.size[0]) // 2  # y coordinate\
-            if self.size[0] < self.size[1]:
-                self.size[0], self.size[1] = self.size[1], self.size[0]
 
-    def turnupdown(self):
-        self.location[0] = self.location[0] + abs(self.size[1] - self.size[0]) // 2
-        self.location[1] = self.location[1] - abs(self.size[1] - self.size[0]) // 2
-        # if self.size[0] > self.size[1]:
-        #    self.size[0], self.size[1] = self.size[1], self.size[0]
-
-
-MainTank = Tank([250, 250], [80, 100], 10)
-
-
+MainTank = Tank([250, 250], 32, 32, 10, up=True)
 def redrawWindows():
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (MainTank.location[0], MainTank.location[1], MainTank.size[0], MainTank.size[1]))
+    win.blit(bg, (0, 0))
+    if MainTank.left or MainTank.right:
+        win.blit(facesideway, (MainTank.location[0], MainTank.location[1]))
+
+    if MainTank.up or MainTank.down:
+        win.blit(faceupdown, (MainTank.location[0], MainTank.location[1]))
+
     pygame.display.update()
 
 
@@ -70,23 +58,19 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and MainTank.location[0] > 0:
-        if not MainTank.left:
-            MainTank.turn()
         MainTank.left = True
         MainTank.right = False
         MainTank.up = False
         MainTank.down = False
+
         MainTank.move()
 
-
-
-
-    if keys[pygame.K_RIGHT] and MainTank.location[0] < window[0] - MainTank.size[0]:
+    if keys[pygame.K_RIGHT] and MainTank.location[0] < window[0] - MainTank.width:
         MainTank.left = False
         MainTank.right = True
         MainTank.up = False
         MainTank.down = False
-        #MainTank.turn()
+
         MainTank.move()
 
     if keys[pygame.K_UP] and MainTank.location[1] > 0:
@@ -94,18 +78,16 @@ while run:
         MainTank.right = False
         MainTank.up = True
         MainTank.down = False
-        #MainTank.turnupdown()
-        MainTank.move()
-        # MainTank.turn(lr=False)
 
-    if keys[pygame.K_DOWN] and MainTank.location[1] < window[1] - MainTank.size[1]:
+        MainTank.move()
+
+    if keys[pygame.K_DOWN] and MainTank.location[1] < window[1] - MainTank.height:
         MainTank.left = False
         MainTank.right = False
         MainTank.up = False
         MainTank.down = True
-        #MainTank.turnupdown()
+
         MainTank.move()
-        # MainTank.turn(lr=False)
 
     redrawWindows()
 
