90252ef5bfa5c701ab9829a808f355e588d71042
trungne <nguyenquochoangtrung@gmail.com>
2020-04-23 07:56:55 +0700

    van chua rotate dc

--- TankIt.py
+++ TankIt.py
@@ -12,44 +12,50 @@ pygame.display.set_caption("TankIt")
 
 # character - tank
 class Tank:
-    def __init__(self, location, size, vel):
+    def __init__(self, location, size, vel, left=False, right=False, up=False, down=False):
         self.location = location
-        self.size = size # default 0 = width, 1 = height
+        self.size = size  # default 0 = width, 1 = height
         self.vel = vel
+        self.left = left
+        self.right = right
+        self.up = up
+        self.down = down
 
-    def move(self, left=False, right=False, up=False, down=False):
-        if left:
+    def move(self):
+        if self.left:
             self.location[0] -= self.vel
-        elif right:
+        elif self.right:
             self.location[0] += self.vel
-        elif up:
+        elif self.up:
             self.location[1] -= self.vel
-        elif down:
+        elif self.down:
             self.location[1] += self.vel
 
     def turn(self):  # left + right
-        self.location[0] = self.location[0] - abs(self.size[1] - self.size[0]) // 2  # x coordinate
-        self.location[1] = self.location[1] + abs(self.size[1] - self.size[0]) // 2  # y coordinate
-        if self.size[0] < self.size[1]:
-            self.size[0], self.size[1] = self.size[1], self.size[0]
+        if not self.left:
+            self.location[0] = self.location[0] - abs(self.size[1] - self.size[0]) // 2  # x coordinate
+            self.location[1] = self.location[1] + abs(self.size[1] - self.size[0]) // 2  # y coordinate
+            if self.size[0] < self.size[1]:
+                self.size[0], self.size[1] = self.size[1], self.size[0]
+        if not self.right:
+            self.location[0] = self.location[0] - abs(self.size[1] - self.size[0]) // 2  # x coordinate
+            self.location[1] = self.location[1] + abs(self.size[1] - self.size[0]) // 2  # y coordinate\
+            if self.size[0] < self.size[1]:
+                self.size[0], self.size[1] = self.size[1], self.size[0]
+
     def turnupdown(self):
         self.location[0] = self.location[0] + abs(self.size[1] - self.size[0]) // 2
         self.location[1] = self.location[1] - abs(self.size[1] - self.size[0]) // 2
-        if self.size[0] > self.size[1]:
-            self.size[0], self.size[1] = self.size[1], self.size[0]
-    #     else:  # up + down
-    #         self.location[0] = self.location[0] + (self.size[1] - self.size[0])// 2  # x
-    #         self.location[1] = self.location[1] - (self.size[1] - self.size[0])// 2  # y
-    #         #self.size[0], self.size[1] = self.size[1], self.size[0]
+        # if self.size[0] > self.size[1]:
+        #    self.size[0], self.size[1] = self.size[1], self.size[0]
 
 
-MainTank = Tank([50, 50], [80, 100], 20)
+MainTank = Tank([250, 250], [80, 100], 10)
 
 
 def redrawWindows():
     win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0),
-                     (MainTank.location[0], MainTank.location[1], MainTank.size[0], MainTank.size[1]))
+    pygame.draw.rect(win, (255, 0, 0), (MainTank.location[0], MainTank.location[1], MainTank.size[0], MainTank.size[1]))
     pygame.display.update()
 
 
@@ -64,22 +70,41 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and MainTank.location[0] > 0:
-        MainTank.turn()
-        MainTank.move(left=True)
+        if not MainTank.left:
+            MainTank.turn()
+        MainTank.left = True
+        MainTank.right = False
+        MainTank.up = False
+        MainTank.down = False
+        MainTank.move()
+
+
+
 
     if keys[pygame.K_RIGHT] and MainTank.location[0] < window[0] - MainTank.size[0]:
-        MainTank.turn()
-        MainTank.move(right=True)
-
+        MainTank.left = False
+        MainTank.right = True
+        MainTank.up = False
+        MainTank.down = False
+        #MainTank.turn()
+        MainTank.move()
 
     if keys[pygame.K_UP] and MainTank.location[1] > 0:
-        MainTank.turnupdown()
-        MainTank.move(up=True)
+        MainTank.left = False
+        MainTank.right = False
+        MainTank.up = True
+        MainTank.down = False
+        #MainTank.turnupdown()
+        MainTank.move()
         # MainTank.turn(lr=False)
 
     if keys[pygame.K_DOWN] and MainTank.location[1] < window[1] - MainTank.size[1]:
-        MainTank.turnupdown()
-        MainTank.move(down=True)
+        MainTank.left = False
+        MainTank.right = False
+        MainTank.up = False
+        MainTank.down = True
+        #MainTank.turnupdown()
+        MainTank.move()
         # MainTank.turn(lr=False)
 
     redrawWindows()
