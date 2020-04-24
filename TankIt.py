2d640e783287f8d25680f807ebee4f5664caefca
trungne <nguyenquochoangtrung@gmail.com>
2020-04-24 23:37:38 +0700

    Create tank class, fix boundaries

--- TankIt.py
+++ TankIt.py
@@ -1,6 +1,7 @@
 import pygame
-
+from objects.Tank import Tank
 pygame.init()
+
 # basic display
 window = (640, 640)
 win = pygame.display.set_mode(window)
@@ -9,69 +10,7 @@ clock = pygame.time.Clock()
 
 # images file import:
 bg = pygame.image.load('grass.png')
-faceleft = pygame.image.load('tankBaseleft.png')
-faceright = pygame.image.load('tankBaseright.png')
-faceup = pygame.image.load('tankBaseup.png')
-facedown = pygame.image.load('tankBasedown.png')
 
-
-# character - tank
-class Tank:
-    def __init__(self, location, width, height, velX, velY, accelerationX, accelerationY, dir=[0, -1]):
-        # attributes
-        self.location = location
-        self.width = width
-        self.height = height
-        self.velX = velX
-        self.velY = velY
-        self.accelerationX = accelerationX
-        self.accelerationY = accelerationY
-        self.maxspeed = 6
-
-        # facing
-        self.dir = dir
-
-    def move(self):
-        print(f'{self.location[0]},{self.location[1]}')
-
-        # boundaries
-        if int(self.location[0]) < 0 or int(self.location[0]) > window[0] - self.width:
-            self.accelerationX = 0  # TODO: Fix
-            self.velX = - self.velX
-        if int(self.location[1]) < 0 or int(self.location[1]) > window[1] - self.height:
-            self.accelerationY = 0
-            self.velY = - self.velY
-
-        # constraint
-        if abs(self.velX) < self.maxspeed:
-            self.velX += self.accelerationX
-
-        if abs(self.velY) < self.maxspeed:
-            self.velY += self.accelerationY
-
-        # move object
-        if self.velX:  # left/right movement
-            self.location[0] += self.velX
-
-        if self.velY:  # up/down movement
-            self.location[1] += self.velY
-
-    def decelerate(self):
-        if self.accelerationX == 0:
-            self.velX *= 0.92
-
-        if self.accelerationY == 0:
-            self.velY *= 0.92
-
-    def redrawtank(self):
-        if self.dir == [-1, 0]:
-            win.blit(faceleft, (self.location[0], self.location[1]))
-        elif self.dir == [1, 0]:
-            win.blit(faceright, (self.location[0], self.location[1]))
-        elif self.dir == [0, -1]:
-            win.blit(faceup, (self.location[0], self.location[1]))
-        elif self.dir == [0, 1]:
-            win.blit(facedown, (self.location[0], self.location[1]))
 
 
 MainTank = Tank([250, 250], 32, 32, 0, 0, 0, 0)
@@ -79,7 +18,7 @@ MainTank = Tank([250, 250], 32, 32, 0, 0, 0, 0)
 
 def redrawWindows():
     win.blit(bg, (0, 0))
-    MainTank.redrawtank()
+    MainTank.redrawtank(win)
     pygame.display.update()
 
 
@@ -87,6 +26,7 @@ def redrawWindows():
 run = True
 while run:
     pygame.time.delay(10)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -112,7 +52,9 @@ while run:
     # deceleration
     MainTank.decelerate()
 
-    MainTank.move()
+    # check boundaries
+    MainTank.move(window)
+
     redrawWindows()
 
 pygame.quit()
---
+++ objects/Tank.py
@@ -0,0 +1,65 @@
+import pygame
+pygame.init()
+
+# character - tank
+faceleft = pygame.image.load('tankBaseleft.png')
+faceright = pygame.image.load('tankBaseright.png')
+faceup = pygame.image.load('tankBaseup.png')
+facedown = pygame.image.load('tankBasedown.png')
+class Tank:
+    def __init__(self, location, width, height, velX, velY, accelerationX, accelerationY, dir=[0, -1]):
+        # attributes
+        self.location = location
+        self.width = width
+        self.height = height
+        self.velX = velX
+        self.velY = velY
+        self.accelerationX = accelerationX
+        self.accelerationY = accelerationY
+        self.maxspeed = 6
+
+        # facing
+        self.dir = dir
+
+    def move(self, window):
+        # testing
+        print(f'{self.location[0]},{self.location[1]}')
+
+        # constraint
+        if abs(self.velX) < self.maxspeed:
+            self.velX += self.accelerationX
+
+        if abs(self.velY) < self.maxspeed:
+            self.velY += self.accelerationY
+
+        # move object
+        updated_x = self.location[0] + self.velX
+        if self.velX and 0 < updated_x < window[0] - self.width:  # left/right movement
+            self.location[0] = updated_x
+        else:
+            self.velX = - self.velX
+            self.accelerationX = 0
+
+        updated_y = self.location[1] + self.velY
+        if self.velY and 0 < updated_y < window[1] - self.height:  # up/down movement
+            self.location[1] = updated_y
+        else:
+            self.velY = - self.velY
+            self.accelerationY = 0
+
+    def decelerate(self):
+        if self.accelerationX == 0:
+            self.velX *= 0.98
+
+        if self.accelerationY == 0:
+            self.velY *= 0.98
+
+    def redrawtank(self, win):
+        if self.dir == [-1, 0]:
+            win.blit(faceleft, (self.location[0], self.location[1]))
+        elif self.dir == [1, 0]:
+            win.blit(faceright, (self.location[0], self.location[1]))
+        elif self.dir == [0, -1]:
+            win.blit(faceup, (self.location[0], self.location[1]))
+        elif self.dir == [0, 1]:
+            win.blit(facedown, (self.location[0], self.location[1]))
